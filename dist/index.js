460930b4541f16739e1f25c9af5c57b7d5c18973
Parker Ault <engineering@internetparty.io>
2021-03-12 13:47:25 -0600

    First commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+function app() {
+    return "it works";
+}
+exports.app = app;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEsU0FBZ0IsR0FBRztJQUNqQixPQUFPLFVBQVUsQ0FBQztBQUNwQixDQUFDO0FBRkQsa0JBRUMifQ==
