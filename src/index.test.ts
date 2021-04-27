ba299bfb108a1b0be7f4e3b4a76178c1813776e0
Parker Ault <engineering@internetparty.io>
2021-04-27 15:51:23 -0500

    Use ts-node, use nodemon instead of npm-watch

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import test from "tape-catch";
-import * as App from "./index"
+import * as App from "./index";
 
 test("it runs the tests", (assert) => {
-  assert.plan(1);
-  assert.equals(App.app(), "it works")
+  assert.equals(App.app(), "it works");
+  assert.end();
 });
